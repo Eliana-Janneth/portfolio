d5b0d038f90703940d2bc1ff84f0ec67e9c8a027
Eliana Puerta <80785262+Eliana-Janneth@users.noreply.github.com>
2024-03-31 11:40:52 -0500

    feat: add react-confetti package and implement confetti animation

--- src/app/components/organisms/MainInfo.tsx
+++ src/app/components/organisms/MainInfo.tsx
@@ -2,11 +2,19 @@
 import Image from "next/image";
 import { useState } from "react";
 import { PopUp } from "../molecules/Popup";
+import Confetti from 'react-confetti'
 
-
-export default function Education() {
+export default function MainInfo() {
     const [open, setOpen] = useState(false);
     const closeModal = () => setOpen(false);
+    const [showConfetti, setShowConfetti] = useState(false);
+
+    const handleClick = () => {
+        setShowConfetti(true);
+        setTimeout(() => {
+            setShowConfetti(false);
+        }, 3500);
+    };
 
     return (
         <>
@@ -16,11 +24,19 @@ export default function Education() {
                         <h1 className="text-5xl font-bold"> I&apos;m Eliana Puerta</h1>
                         <h1 className="font-bold text-5xl"><span className="text-pink-500 text-gradient">Front-end</span> Developer</h1>
                     </div>
-                    <p>I&apos;m a passionate software developer, I enjoy creating web applications focused on the frontend and I am constantly exploring new technologies to improve my skills.</p>
-                    <button className="rounded-lg font-semibold w-fit bg-pink-500 text-black py-2 px-6 text-xl flex items-center text-gradient-color" onClick={() => setOpen(o => !o)}>HIRE ME
+                    <p>I&apos;m a passionate software developer, I enjoy creating web applications focused on the frontend and I&apos;m constantly exploring new technologies to improve my skills.</p>
+                    <button className="rounded-lg font-semibold w-fit bg-pink-500 text-black py-2 px-6 text-xl flex items-center text-gradient-color"
+                        onClick={() => {
+                            setOpen(o => !o);
+                            handleClick();
+                        }}
+                    >HIRE ME
                         <span className="icon-[mingcute--arrow-right-fill] pl-6"></span>
                     </button>
+                    {showConfetti && <Confetti />}
+
                     <PopUp open={open} closeModal={closeModal} >
+
                         <p className="text-justify text-base text-pink-50">
                             I&apos;m characterized by being a creative person, with a desire to excel and goals based on objectives;
                             I&apos;m a fast learner and I am interested in fulfilling my work properly, punctually and responsibly.<br />
