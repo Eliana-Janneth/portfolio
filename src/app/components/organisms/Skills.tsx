635ad0bfd136f1ad9d5668439be4ee5026c32733
Eliana Puerta <80785262+Eliana-Janneth@users.noreply.github.com>
2024-04-06 22:50:14 -0500

    fix: responsive carousel bug

--- src/app/components/organisms/Skills.tsx
+++ src/app/components/organisms/Skills.tsx
@@ -5,7 +5,7 @@ import { ProgressBarWithText } from '../molecules'
 //organismo que contiene los componentes moleculas Title y ProgressBarWithText para mostrar los lenguajes
 export default function Skills() {
   return (
-    <div className="flex h-min w-full flex-col">
+    <div className="flex h-min w-full flex-col whitespace-nowrap">
       <Title>Programming Languages</Title>
       <ProgressBarWithText title="JavaScript " value={80}></ProgressBarWithText>
       <ProgressBarWithText title="SQL" value={80}></ProgressBarWithText>
--- src/app/components/templates/MainContent.tsx
+++ src/app/components/templates/MainContent.tsx
@@ -3,7 +3,7 @@ import { Education, Footer, Knowledge, MainInfo, Portfolio } from '../organisms'
 //template que contiene los organismos MainInfo, Knowledge, Education, Portfolio y Footer
 export default function MainContent() {
   return (
-    <main className="color-background-primary flex w-fit flex-1 flex-col items-center justify-center gap-2 px-2">
+    <main className="color-background-primary flex w-fit lg:w-[60%] flex-1 flex-col items-center justify-center gap-2 px-2">
       <MainInfo />
       <Knowledge />
       <Education />
